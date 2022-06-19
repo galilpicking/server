4d4c107d020bfccb52a4634f2a7e1865d56b4857
Oded Kovo <odedkovo2@gmail.com>
2022-06-19 18:35:32 +0300

    third commit

--- server.js
+++ server.js
@@ -3,8 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
-var fs = require('fs');
-var pdf = require('html-pdf');
+import { degrees, PDFDocument, rgb, StandardFonts } from 'pdf-lib';
 
 const app = express();
 const port = 3000;
@@ -14,21 +13,69 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-let html_to_pdf = require('html-pdf-node');
-let options = { format: 'A4' };
-
 app.get('/', (req, res) => {
   res.send('Hello World, from express');
 });
 
-app.post('/returnpdf', async (req, res) => {
+app.post('/safety', async (req, res) => {
   const strHtml = req.body.html;
+  const url = 'https://galilpicking.github.io/safety/';
+  const existingPdfBytes = await fetch(url).then((res) => res.arrayBuffer());
+  const helveticaFont = await pdfDoc.embedFont(StandardFonts.Helvetica);
 
-  pdf.create(strHtml).toBuffer(function (err, buffer) {
-    console.log('This is a buffer:', Buffer.isBuffer(buffer));
-    console.log('This is a buffer:', buffer);
-    res.send(buffer);
+  const pages = pdfDoc.getPages();
+  const firstPage = pages[0];
+  const { width, height } = firstPage.getSize();
+  firstPage.drawText('This text was added with JavaScript!', {
+    x: 5,
+    y: height / 2 + 300,
+    size: 50,
+    font: helveticaFont,
+    color: rgb(0.95, 0.1, 0.1),
+    rotate: degrees(-45),
   });
+
+  const pdfBytes = await pdfDoc.save();
+});
+
+app.post('/rent', async (req, res) => {
+  const strHtml = req.body.html;
+  const url = 'https://galilpicking.github.io/rent/?price=725';
+  const existingPdfBytes = await fetch(url).then((res) => res.arrayBuffer());
+  const helveticaFont = await pdfDoc.embedFont(StandardFonts.Helvetica);
+  const pages = pdfDoc.getPages();
+  const firstPage = pages[0];
+  const { width, height } = firstPage.getSize();
+  firstPage.drawText('This text was added with JavaScript!', {
+    x: 5,
+    y: height / 2 + 300,
+    size: 50,
+    font: helveticaFont,
+    color: rgb(0.95, 0.1, 0.1),
+    rotate: degrees(-45),
+  });
+
+  const pdfBytes = await pdfDoc.save();
+});
+
+app.post('/work', async (req, res) => {
+  const strHtml = req.body.html;
+  const url = 'https://galilpicking.github.io/work/';
+  const existingPdfBytes = await fetch(url).then((res) => res.arrayBuffer());
+  const helveticaFont = await pdfDoc.embedFont(StandardFonts.Helvetica);
+  const pages = pdfDoc.getPages();
+  const firstPage = pages[0];
+  const { width, height } = firstPage.getSize();
+  firstPage.drawText('This text was added with JavaScript!', {
+    x: 5,
+    y: height / 2 + 300,
+    size: 50,
+    font: helveticaFont,
+    color: rgb(0.95, 0.1, 0.1),
+    rotate: degrees(-45),
+  });
+
+  const pdfBytes = await pdfDoc.save();
 });
 
 app.listen(process.env.PORT || port, () =>
